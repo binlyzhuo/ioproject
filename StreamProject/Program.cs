bdc6bb3a7fc0c5e955ebe9ca42dbd2a576842b92
binlyzhuo <binlyzhuo@outlook.com>
2015-12-17 21:25:41 +0800

    ~commit IOProject.sln

---
+++ StreamProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream stream;
+
+            byte[] buffer = null;
+
+            string testString = "Stream!Hello world";
+            char[] readCharArray = null;
+            byte[] readBuffer = null;
+            string readString = string.Empty;
+
+            Console.WriteLine("IO PRoject");
+            Console.ReadLine();
+        }
+    }
+}
