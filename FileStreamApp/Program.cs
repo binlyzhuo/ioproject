c01775938638d3697e689ea3880c4745577844d1
binlyzhuo <binlyzhuo@outlook.com>
2015-12-30 17:24:20 +0800

    ~commit

--- FileStreamApp/Program.cs
+++ FileStreamApp/Program.cs
@@ -9,22 +9,48 @@ namespace FileStreamApp
 {
     class Program
     {
+        //http://www.cnblogs.com/liuxinls/archive/2013/02/15/2912968.html
+        //http://www.wxzzz.com/1687.html
+        //http://www.wxzzz.com/683.html
+        //http://www.wxzzz.com/1607.html
+        //http://www.cnblogs.com/Mr_JinRui/archive/2010/07/05/1771184.html
         static void Main(string[] args)
         {
-            Console.WriteLine("Just a test for the stream!!");
-            string filePath = @"d:\2.txt";
-            FileStream fs = new FileStream(filePath,FileMode.OpenOrCreate);
-            int nextByte = fs.ReadByte();
-            Console.WriteLine("Next Byte:"+nextByte);
-            byte[] reader = new byte[8];
-            fs.Close();
+            //Console.WriteLine("Just a test for the stream!!");
+            //string filePath = @"d:\2.txt";
+            //FileStream fs = new FileStream(filePath,FileMode.OpenOrCreate);
+            //int nextByte = fs.ReadByte();
+            //Console.WriteLine("Next Byte:"+nextByte);
+            //byte[] reader = new byte[8];
+            //fs.Close();
 
-            //StringCollection result;
-            var ds = DriveInfo.GetDrives();
-            foreach(var di in ds)
-            {
-                Console.WriteLine(di.Name);
-            }
+            ////StringCollection result;
+            //var ds = DriveInfo.GetDrives();
+            //foreach(var di in ds)
+            //{
+            //    Console.WriteLine(di.Name);
+            //}
+
+            // read file
+            FileStream file = File.Open(@"e:\stream\1.txt",FileMode.Open);
+            byte[] array = new byte[file.Length];
+            file.Read(array, 0, array.Length);
+
+            file.Close();
+
+            string str = Encoding.Default.GetString(array);
+            Console.WriteLine(str);
+
+            // write file
+            FileStream writeFile = new FileStream(@"e:\stream\2.txt",FileMode.Append);
+            byte[] arrTxt = Encoding.UTF8.GetBytes("Hello World!你好");
+            writeFile.Write(arrTxt, 0, arrTxt.Length);
+
+            // 清空缓冲区
+            writeFile.Flush();
+            writeFile.Close();
+
+            //
             Console.ReadLine();
         }
     }
