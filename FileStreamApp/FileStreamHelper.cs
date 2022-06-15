47fea7c8dd290a3e361bbeaea82371743ce77f12
binlyz <binlyzhuo@outlook.com>
2022-06-15 11:36:16 +0800

    ~

---
+++ FileStreamApp/FileStreamHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamApp
+{
+    public class FileStreamHelper
+    {
+        public static void ReadFile()
+        {
+            string file = "WriteFile2.txt";
+
+        }
+    }
+}
